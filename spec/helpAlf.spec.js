62126cb9cb75285f0c9cfd903f11b99df597078c
momotheii <momot@MomosPc.com>
2026-06-11 21:40:39 +0200

    Add test for map without spaceship

--- spec/helpAlf.spec.js
+++ spec/helpAlf.spec.js
@@ -3,3 +3,12 @@ const { findSpaceship } = require("../src/helpAlf");
 test('empty map returns "Spaceship lost forever."', () => {
   expect(findSpaceship("")).toBe("Spaceship lost forever.");
 });
+
+test('map without spaceship returns "Spaceship lost forever."', () => {
+  const map =
+`..........
+..........
+..........`;
+
+  expect(findSpaceship(map)).toBe("Spaceship lost forever.");
+});
